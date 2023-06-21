f7bdec7705229b39971716ebf11fc823b7c7fdb6
AnonimniyGeniy <68807643+AnonimniyGeniy@users.noreply.github.com>
2023-06-21 23:55:16 +0300

    sss

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import csv
 import pandas as pd
 import request_handler
 import models
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -14,14 +16,15 @@ def main():
     events = []
     for line in open('data/events.txt', 'r', encoding='utf-8'):
         events.append(line.strip())
-    #print(events)
-    recommendations = request_handler.generate_recommendations(str(people[0]), events)
+    # print(events)
+    # recommendations = request_handler.generate_recommendation(str(people[0]), events)
+    # for i in recommendations:
+    #     print(i)
+    recommendations = request_handler.generate_recommendations(people[:1], events)
     for i in recommendations:
         print(i)
-
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
--- request_handler.py
+++ request_handler.py
@@ -1,11 +1,11 @@
 import openai
 import models
+import time
 # Set up OpenAI API
 openai.api_key = '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'
 
 
-def generate_recommendations(person, events):
-
+def generate_recommendation(person, events):
     prompt = f"Person: {person}\nAvailable events: {events}\nGenerate event recommendations in a bullet list format. " \
              f"Leave event names unchanged.\nLeave 4 most relevant events for specific person.\n"
     print(prompt)
@@ -22,4 +22,39 @@ def generate_recommendations(person, events):
     # Send prompt to ChatGPT API for generating recommendations
 
     recommendations = [choice['text'].strip() for choice in response.choices]
+    return recommendations
+
+
+def generate_recommendations(persons, events):
+    # prompt = f"Person: {person}\nAvailable events: {events}\nGenerate event recommendations in a bullet list format. Leave event names unchanged.\nLeave 4 most relevant events for specific person.\n"
+    # print(prompt)
+    messages = [
+        {"role": "system",
+         "content": "Generate event recommendations for each provided person in a bullet list format. Leave event "
+                    "names unchanged.\nLeave 4 most relevant events for specific person."},
+        {"role": "system", "content": f"Available events: {events}"},
+    ]
+    recommendations = []
+    for person in persons:
+        messages.append({"role": "user", "content": f"Person: {person}"})
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo-16k',
+            messages=messages,
+            max_tokens=500,
+            temperature=0.7,
+            n=1,
+            timeout=60  # Увеличение времени ожидания ответа до 60 секунд
+        )
+        recommendations += [persons.index(person), [choice['message']['content'] for choice in response.choices]]
+        print(recommendations[-1])
+        time.sleep(20)
+
+        messages = [
+        {"role": "system",
+         "content": "Generate event recommendations for each provided person in a bullet list format. Leave event names unchanged.\nLeave 4 most relevant events for specific person."},
+        {"role": "system", "content": f"Available events: {events}"},
+    ]
+
+    # Send prompt to ChatGPT API for generating recommendations
+
     return recommendations
